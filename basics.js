5f9ef711180598d7b5ff75c5490a098ac0fc569c
Ali Neysaz <alineysaz1387@gmail.com>
2021-11-11 21:16:12 +0330

    Timer

--- basics.js
+++ basics.js
@@ -1,8 +1,51 @@
-let age = 14;
-let person = {
-    name: 'Ali'
-    age:14;
-    
-};
+// console.log("Hello guys!");
+// alert("Hi!");
 
-console.log(person);
+
+
+
+
+// variables
+
+//var b = "sheeeeeeeeeeish";
+//console.log(b)
+//var SomeNumber = 1234566789909876543
+//console.log(SomeNumber)
+
+
+
+
+
+// change the HTML code
+
+
+// document.getElementById("1").innerHTML = "Hello guys"
+
+
+
+
+
+// ask for sth (in this case, the age will be typed in the web browser)
+
+
+
+//var age = prompt("what is your age?");
+//document.getElementById("1").innerHTML = age;
+
+
+
+
+
+
+
+
+
+
+
+
+// Numbers in js
+
+
+var num1 = 10;
+num1 = num1 + 1;
+console.log(num1);
